622c98f66989b6b5c56901471b2874ddc0d6f60c
BuTq3b <BuTq3b69@yandex.ru>
2023-10-09 14:38:48 +0300

    Семинар 2

---
+++ task12 два числа и проверка на квадрат/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 12.Напишите программу, которая будет принимать 
+на вход два числа и выводить, является ли второе 
+число кратным первому. 
+Если второе число некратно первому, то программа
+выводит остаток от деления.
+34, 5 -> некратно, остаток 4
+16, 4 -> кратно
+*/
+System.Console.WriteLine("Введите 2 числа: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA/numberB == numberB)
+{
+    System.Console.WriteLine("кратно");
+}
+else
+{
+        System.Console.WriteLine($"некратно, остаток {numberA%numberB}");
+}
---
+++ task16 квадрат числа в обе сотроны/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 16.Напишите программу, которая принимает на вход 
+два числа и проверяет, 
+является ли одно число квадратом другого.
+5, 25 -> да
+-4, 16 -> да
+25, 5 -> да
+8,9 -> нет
+*/
+
+System.Console.WriteLine("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA/numberB == numberB || numberB/numberA == numberA)
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("нет");
+}
