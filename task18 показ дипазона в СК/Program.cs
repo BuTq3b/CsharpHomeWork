8c7439959851ac800242454c45c28f26f9318d68
BuTq3b <BuTq3b69@yandex.ru>
2023-10-13 10:46:19 +0300

    решена домашка Семинар 3

---
+++ task18 показ дипазона в СК/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 18. Напишите программу, которая по заданному номеру 
+четверти, показывает диапазон
+возможных координат точек в этой четверти (x и y).
+*/
+Console.Clear();
+Console.Write("Введи номер четверти: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 1) System.Console.WriteLine("x > 0 и y > 0");
+if (number == 2) System.Console.WriteLine("x < 0 и y > 0");
+if (number == 3) System.Console.WriteLine("x < 0 и y < 0");
+if (number == 4) System.Console.WriteLine("x > 0 и y < 0");
+if (number < 1 || number > 4) 
+{
+    System.Console.WriteLine("Введено неверное значение");
+}
---
+++ task20 расстояние между точками 2D/Program.cs
@@ -0,0 +1,21 @@
+﻿/* B0. Напишите программу, которая принимает на вход 
+координаты двух точек и находит
+расстояние между ними в BD пространстве.
+
+
+*/
+
+Console.Clear();
+System.Console.Write("Введи координату XA: ");
+int numXA = int.Parse(Console.ReadLine());
+System.Console.Write("Введи координату YA: ");
+int numYA = int.Parse(Console.ReadLine());
+System.Console.Write("Введи координату XB: ");
+int numXB = int.Parse(Console.ReadLine());
+System.Console.Write("Введи координату YB: ");
+int numYB = int.Parse(Console.ReadLine());
+
+double sqrX = Math.Pow(numXB - numXA, 2); // функция возводит в степень, нерез запятую указывается степеь
+double sqrY = Math.Pow(numYB - numYA, 2);
+double sqrt = Math.Sqrt(sqrX + sqrY); // Корень квадратный
+System.Console.WriteLine(Math.Round(sqrt,2)); // Math.Round(sqrt,2) округляет знаки послезапятой (переменная, сколько знаков)
