ca306cec54e061db57419f48609e835559c40e20
BuTq3b <BuTq3b69@yandex.ru>
2023-10-21 15:51:48 +0300

    дз семинар 5

---
+++ task34 кол-во четных элементов в массиве/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Напишите программу, которая подсчитывает количество четных 
+элементов в массиве целых положительных 
+трехзначных чисел и выводит результат на экран.
+*/
+Console.Clear();
+Console.Write("Задайте длину массива: ");
+int size = int.Parse(Console.ReadLine());
+int[] Arrey = NewRandomArrey(size);
+PrintAr(Arrey);
+System.Console.WriteLine();
+PrintResult(Arrey);
+
+
+int[] NewRandomArrey(int length)
+{
+    int[] newA = new int[length];
+
+    for (int i = 0; i < newA.Length; i++)
+    {
+        newA[i] = new Random().Next(100, 1000);
+    }
+    return newA;
+}
+
+void PrintAr(int[] ar)
+{
+    foreach (var i in ar)
+    {
+        System.Console.Write(i + " ");
+    }
+}
+
+void PrintResult(int[] ar)
+{
+    int sum = 0;
+    for (int i = 0; i < ar.Length; i++)
+    {
+        if (ar[i] % 2 == 0)
+        {
+            System.Console.WriteLine($"{ar[i]} - ЧЕТНЫЙ ЭЛЕМЕНТ");
+            sum ++;
+        }
+    }
+    System.Console.WriteLine($"Количество четных элементов {sum}");
+}
---
+++ task36 сумма нечетных индексов/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Напишите программу, которая находит сумму элементов с нечетными индексами 
+в одномерном массиве целых чисел и выводит результат на экран.
+*/
+Console.Clear();
+Console.Write("Задайте длину массива: ");
+int size = int.Parse(Console.ReadLine());
+int[] Arrey = NewRandomArrey(size);
+PrintAr(Arrey);
+System.Console.WriteLine();
+PrintResult(Arrey);
+
+
+int[] NewRandomArrey(int length)
+{
+    int[] newA = new int[length];
+
+    for (int i = 0; i < newA.Length; i++)
+    {
+        newA[i] = new Random().Next(100, 1000);
+    }
+    return newA;
+}
+
+void PrintAr(int[] ar)
+{
+    foreach (var i in ar)
+    {
+        System.Console.Write(i + " ");
+    }
+}
+
+void PrintResult(int[] ar)
+{
+    int sum = 0;
+    for (int i = 1; i < ar.Length; i += 2)
+    {
+        
+        sum += ar[i];
+
+    }
+    System.Console.Write($"Сумма элементов массива с нечетным индексом = {sum}");
+}
