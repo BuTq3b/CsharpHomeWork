18fc8b364caba995c2eb6a984d6013c8d10f1c1b
BuTq3b <BuTq3b69@yandex.ru>
2023-10-26 14:51:46 +0300

    Семинар 7

---
+++ task41dz/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+int[] arr = new int[8];
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(-99, 100);
+    if (arr[i] > 0) {count++;}
+}
+
+
+void PrintArrey(int[] arrey)
+{
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        Console.Write(arrey[i] + " ");
+    }
+}
+
+PrintArrey(arr);
+System.Console.WriteLine();
+
+
+System.Console.WriteLine($"Положительных чисел: {count}");
---
+++ task51 диагональ/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+51. Задайте двумерный массив. Найдите сумму элементов, 
+находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+*/
+
+Console.Clear();
+
+int[,] Get2DArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write(arr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int FundSum(int[,] arr)
+{
+    int sum = 0;
+    
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i==j) 
+            {sum += arr[i,j];}
+        }
+    }
+    return sum;
+
+}
+
+int[,] user2DArray = Get2DArray(5, 7);
+Print2DArray(user2DArray);
+System.Console.WriteLine();
+int sum = FundSum(user2DArray);
+System.Console.WriteLine(sum);
